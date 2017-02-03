089b0b632f10dfe5dfbd79bcda88af27f62ebb99
ordasf <fordas88@gmail.com>
2017-02-03 16:13:39 +0100

    First modification, tests of the comunication with the API and using gradle to store the api key

--- app/src/main/java/com/project/movies/popular/popularmovies1/MainActivity.java
+++ app/src/main/java/com/project/movies/popular/popularmovies1/MainActivity.java
@@ -1,13 +1,59 @@
 package com.project.movies.popular.popularmovies1;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+import com.project.movies.popular.popularmovies1.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mMovieTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMovieTextView = (TextView) findViewById(R.id.tv_movie_data);
+
+        FetchMovieTask movieTask = new FetchMovieTask();
+        movieTask.execute("");
+
+    }
+
+    public class FetchMovieTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            URL movieUrl = NetworkUtils.buildUrl("");
+
+            String response = null;
+            try {
+                response = NetworkUtils.getResponseFromHttpUrl(movieUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            mMovieTextView.setText(result);
+        }
+
     }
 }
---
+++ app/src/main/java/com/project/movies/popular/popularmovies1/utilities/NetworkUtils.java
@@ -0,0 +1,85 @@
+package com.project.movies.popular.popularmovies1.utilities;
+
+import android.net.Uri;
+import android.util.Log;
+
+import com.project.movies.popular.popularmovies1.BuildConfig;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * These utilities will be used to communicate with the movie db servers. The original NetworkUtils
+ * class were using in the exercises of the Fast Track Android Development course and I have adapted
+ * the class to perform the communication with the The Movie Database API
+ */
+public final class NetworkUtils {
+
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String POPULAR_MOVIES_URL =
+            "http://api.themoviedb.org/3/movie/";
+
+    private static final String POPULAR_ORDER = "popular";
+
+    private static final String TOP_RATED_ORDER = "top_rated";
+
+    private static final String API_KEY_QUERY = "api_key";
+
+    private static final String MOVIE_BASE_URL = POPULAR_MOVIES_URL + POPULAR_ORDER;
+
+    final static String KEY = BuildConfig.API_KEY;
+
+    /**
+     * Builds the URL used to talk to the movie db server using an order type.
+     *
+     * @param orderType The type or order to append to the query
+     * @return The URL to use to query the movie db server.
+     */
+    public static URL buildUrl(String orderType) {
+        Uri builtUri = Uri.parse(MOVIE_BASE_URL).buildUpon()
+                .appendQueryParameter(API_KEY_QUERY, KEY)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URI " + url);
+
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
